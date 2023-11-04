cdd97eaa6c300250174e3e183e886c0ddc2dd8ac
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2023-11-04 16:51:41 +0530

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -1,15 +1,16 @@
 import requests
 from geopy.geocoders import Nominatim
-
-def get_lat_lng(name: str) -> tuple:
+# it get latitude and longitude value by area name 
+def get_lat_lng(city: str) -> tuple:
    geolocator = Nominatim(user_agent="your_app_name")
-   location = geolocator.geocode(name)
+   location = geolocator.geocode(city)
    if location:
       latitude = round(location.latitude, 3)
       longitude = round(location.longitude, 3)
       return latitude, longitude
    return 0, 0
 
+# it gets water quality index from api using api_key and return that data 
 def wqi_data(city: str) -> dict:
    api_key = "4FYIeuwcXmpspTIjK4fkK8BJJIHSpwuL"
    lat, lng = get_lat_lng(city)
@@ -24,12 +25,16 @@ def wqi_data(city: str) -> dict:
          return data
    return {"found": False}
 
+# Enter the city name 
 city = input("city name: ").lower().strip()
+# get the  wqi details using api details
+
 details = wqi_data(city)
 
-# parse the data 
+# parse the data and print it on screen you can manuplate it accoding to what do you want to do with this data.
 if details['found']:
    print("\n")
+   print(f"Date time: {details['datetime']}")
    print(f"Index type: {details['index']['index_type']}")
    print(f"Overall Qualificaton: {details['index']['qualification']}")
    print(f"Total index value: {details['index']['value']}")
